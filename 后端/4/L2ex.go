2361aeb8c51b4ed86e2b111deafc3512fba9b7b5
ypy2021212672 <2259202040@qq.com>
2022-11-05 18:34:10 +0800

    L2ex更改

--- 后端/4/L2ex.go
+++ 后端/4/L2ex.go
@@ -10,6 +10,7 @@ type Ticker1 struct {
 	ticker *time.Ticker
 	time   int
 	info   string
+	doing  bool
 }
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 	var tickers []Ticker1
 	for {
 		var x int
-		fmt.Println("1.增加定时器,2.删除定时器，3.查看定时器,4.启动定时器，5.,输入其它按键就退出！")
+		fmt.Println("1.增加定时器,2.删除定时器，3.查看定时器,4.启动定时器,5.停止计时器   输入其它按键就退出！")
 		_, err := fmt.Scanln(&x)
 		if err != nil {
 		}
@@ -29,7 +30,9 @@ func main() {
 		case 3:
 			viewTicker(tickers)
 		case 4:
-			startTicker(tickers)
+			startTicker(&tickers)
+		case 5:
+			closeTicker(&tickers)
 		default:
 			fmt.Println("退出中......")
 			time.Sleep(time.Second * 2)
@@ -52,9 +55,8 @@ func buildTicker(tickers *[]Ticker1) {
 	}
 	t := time.NewTicker(time.Second * time.Duration(s))
 	ticker := Ticker1{
-		t, s, name,
+		t, s, name, false,
 	}
-	fmt.Println(ticker)
 	*tickers = append(*tickers, ticker)
 }
 func viewTicker(tickers []Ticker1) {
@@ -75,37 +77,35 @@ re:
 		fmt.Println("你输入的序号错了")
 		goto re
 	}
-	i++
-	ticker1 := (*tickers)[i:]
-	*tickers = append((*tickers)[:i-1], ticker1...)
+	*tickers = append((*tickers)[:i-1], (*tickers)[i:]...)
 }
-func startTicker(tickers []Ticker1) {
+func startTicker(tickers *[]Ticker1) {
 restart:
 	var j, g int
 	fmt.Println("你要启动计时器的序号")
 	_, err := fmt.Scanln(&j)
-	if err != nil || j < 1 || j > len(tickers) {
+	if err != nil || j < 1 || j > len(*tickers) {
 		println("请输入正确序号")
 		goto restart
 	}
-	for i, ticker1 := range tickers {
+	for i, _ := range *tickers {
 		if i == j-1 {
 			fmt.Println("输入1个数字任意退出计时器")
 			var k rune
-			ticker := time.NewTicker(time.Second * time.Duration(ticker1.time))
+			(*tickers)[i].ticker = time.NewTicker(time.Second * time.Duration((*tickers)[i].time))
 			go func() {
 				for {
 					select {
-					case <-ticker.C:
+					case <-(*tickers)[i].ticker.C:
 						g++
-						fmt.Println(ticker1.info, "----正在计时中....第", g, "次")
+						(*tickers)[i].doing = true
+						fmt.Println((*tickers)[i].info, "----正在计时中....第", g, "次")
 					}
 				}
 			}()
 			for {
 				n, err := fmt.Scanln(&k)
 				if n != 0 {
-					ticker.Stop()
 					return
 				}
 				if err != nil {
@@ -113,4 +113,18 @@ restart:
 			}
 		}
 	}
+}
+func closeTicker(tickers *[]Ticker1) {
+	fmt.Println("现在正在运行的计时器有")
+	for i, ticker := range *tickers {
+		if ticker.doing {
+			fmt.Println(i+1, ticker.info)
+		}
+	}
+	fmt.Println("你要停止哪一个计时器")
+	var x int
+	fmt.Scanln(&x)
+	(*tickers)[x-1].ticker.Stop()
+	(*tickers)[x-1].doing = false
+	fmt.Println((*tickers)[x-1].info, "已经停止")
 }
